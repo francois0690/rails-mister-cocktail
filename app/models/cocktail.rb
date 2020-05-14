02a7c9b1048218b60e9809f6f059719db745504c
Francois Jouvray <francois0690@gmail.com>
2020-05-14 20:32:51 +0200

    sf update

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,5 @@ class Cocktail < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
-  has_one_attached :photo
+  has_one_attached :photo, dependent: :destroy
 end
